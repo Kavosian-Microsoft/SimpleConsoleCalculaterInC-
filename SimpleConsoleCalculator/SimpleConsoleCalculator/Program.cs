896cfd26f9c26079d8cb46b9a6c1c1d49e43bfaa
Yashar kavosian <kavoosian@hotmail.com>
2020-03-06 18:43:19 +0330

    Simple calculator is built

--- SimpleConsoleCalculator/SimpleConsoleCalculator/Program.cs
+++ SimpleConsoleCalculator/SimpleConsoleCalculator/Program.cs
@@ -10,6 +10,17 @@ namespace SimpleConsoleCalculator
     {
         static void Main(string[] args)
         {
+            double a, b, c;
+            Console.Title = "Simple Calculator";
+            Console.Write("\n\tEnter A:");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("\n\tEnter B:");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("\n\tA+B={0}", a + b);
+            Console.Write("\n\tA-B={0}", a - b);
+            Console.Write("\n\tAxB={0}", a * b);
+            Console.Write("\n\tA/B={0}", a / b);
+            Console.ReadKey();
         }//Main
     }//Program
 }//SimpleConsoleCalculator
